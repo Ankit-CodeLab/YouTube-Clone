945c213964aba4549b76097b39539617c1c7cc32
Ankit Kushwaha <143326279+Ankit-CodeLab@users.noreply.github.com>
2023-08-31 15:19:51 +0530

    Update index.js

--- Resources/index.js
+++ Resources/index.js
@@ -6,3 +6,68 @@ menuicon.onclick = function(){
     sidebar.classList.toggle("hidden-sidebar")
     mainbox.classList.toggle("large-main-box")
 }
+
+function hide() {
+
+var div = document.getElementById("hide");  
+var div2 = document.getElementById("hide2");  
+var div3 = document.getElementById("hide3"); 
+var show = document.getElementById("show"); 
+var show2 = document.getElementById("show2"); 
+var show3 = document.getElementById("show3"); 
+var close = document.getElementById("search-close"); 
+var close2 = document.getElementById("close"); 
+
+    if(div.style.display === "none") {
+        div.style.display = "flex";
+        div2.style.display = "block";
+        div3.style.display = "flex";
+        show.style.display = "none";
+        show2.style.display = "none";
+        show3.style.display = "none";
+        close.style.display = "none";
+        close2.style.display = "none";
+    }
+    else {
+        div.style.display = "none";
+        div2.style.display = "none";
+        div3.style.display = "none";
+        show.style.display = "flex";
+        show2.style.display = "block";
+        show3.style.display = "block";
+        close.style.display = "block";
+        close2.style.display = "block";
+    }
+}
+
+function show() {
+    var div = document.getElementById("hide");  
+var div2 = document.getElementById("hide2");  
+var div3 = document.getElementById("hide3"); 
+var show = document.getElementById("show"); 
+var show2 = document.getElementById("show2"); 
+var show3 = document.getElementById("show3"); 
+var close = document.getElementById("search-close"); 
+var close2 = document.getElementById("close"); 
+
+    if(div.style.display === "none") {
+        div.style.display = "flex";
+        div2.style.display = "flex";
+        div3.style.display = "flex";
+        show.style.display = "none";
+        show2.style.display = "none";
+        show3.style.display = "none";
+        close.style.display = "none";
+        close2.style.display = "none";
+    }
+    else {
+        div.style.display = "none";
+        div2.style.display = "none";
+        div3.style.display = "none";
+        show.style.display = "flex";
+        show2.style.display = "block";
+        show3.style.display = "block";
+        close.style.display = "block";
+        close2.style.display = "block";
+    }
+}
