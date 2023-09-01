af62dc3d36e1892e773b8a08f54f12374f5d1720
Ankit Kushwaha <143326279+Ankit-CodeLab@users.noreply.github.com>
2023-09-01 11:42:42 +0530

    Update index.js

--- Resources/index.js
+++ Resources/index.js
@@ -1,3 +1,5 @@
+/*----------------------------------------------------Side Bar-----------------------------------------------------*/
+
 var menuicon = document.querySelector(".menu")
 var sidebar = document.querySelector(".sidebar")
 var mainbox = document.querySelector(".main-box")
@@ -6,6 +8,8 @@ menuicon.onclick = function(){
     sidebar.classList.toggle("hidden-sidebar")
     mainbox.classList.toggle("large-main-box")
 }
+
+/*---------------------------------------------------Search Bar----------------------------------------------------*/
 
 function hide() {
 
@@ -42,13 +46,13 @@ var close2 = document.getElementById("close");
 
 function show() {
     var div = document.getElementById("hide");  
-var div2 = document.getElementById("hide2");  
-var div3 = document.getElementById("hide3"); 
-var show = document.getElementById("show"); 
-var show2 = document.getElementById("show2"); 
-var show3 = document.getElementById("show3"); 
-var close = document.getElementById("search-close"); 
-var close2 = document.getElementById("close"); 
+    var div2 = document.getElementById("hide2");  
+    var div3 = document.getElementById("hide3"); 
+    var show = document.getElementById("show"); 
+    var show2 = document.getElementById("show2"); 
+    var show3 = document.getElementById("show3"); 
+    var close = document.getElementById("search-close"); 
+    var close2 = document.getElementById("close"); 
 
     if(div.style.display === "none") {
         div.style.display = "flex";
@@ -72,7 +76,8 @@ var close2 = document.getElementById("close");
     }
 }
 
-
+var creatorName = "ANKIT";
+    document.getElementById("creator").textContent = "Page created by: " + creatorName;
 
 const searchInput = document.getElementById('show2');
 const searchButton = document.getElementById('show3');
